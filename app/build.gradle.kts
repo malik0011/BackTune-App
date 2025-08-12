38f0a71da8c1c084d27992c67a0cac6c862f7987
Ayan Malik <ayanmalik.8334@gmail.com>
2025-08-12 16:54:29 +0530

    Release 5(1.0.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.malikstudios.backtune"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // Inject API Key as BuildConfig field
